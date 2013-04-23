cc1860208abf0e8fe55972cb75ba39094eac4bb3
andyrofl <admin@andyrofl.com>
2013-04-23 03:23:37 -0400

    fixed id handling (issue 4) and general improvements.

--- blog/post.php
+++ blog/post.php
@@ -2,14 +2,17 @@
 	include('../sql.php');
 	include('../scripts/validateinput.php');
 
-	//if($_POST['id'] != null){
+	if(array_key_exists('id', $_GET) && $_GET['id'] != null){
 		//TODO don't trust client input, can't even trust my input
 		$res = mysql_fetch_array(mysql_query('SELECT * FROM blog WHERE id='.makesafe($_GET['id'])));
-	//}
+		if($res != null){
+			$valid = true;
+		}
+	}
 ?>
 <html>
 	<head>
-		<title>andyrofl.com | <?php echo($res['title']);?></title>
+		<title>andyrofl.com | <?php if($valid){echo($res['title']);}else{echo('post not found');}?></title>
 		<link rel=StyleSheet href='../styles/main.css' type='text/css'>
 		<link rel=StyleSheet href='blog.css' type='text/css'>
 		<meta charset='utf-8'>
@@ -19,9 +22,19 @@
 			<?php include('../template/header.php');mysql_close($con);?>
 			<div id='content'>
 				<div id='left'>
-					<div id='title'><?php echo($res['title']);?></div>
+					<?php
+						if($valid){
+							echo('<div id="title">'.$res['title'].'</div>
+							<div id="postcontent">'.$res['content'].'</div>
+							<div id="timestamp">'.$res['date'].'</div>');
+						}
+						else{
+							echo('Error. No parameter or invalid parameter for post id. <a href="/blog/">Click here</a> to return to blog index');
+						}
+					?>
+					<!-- div id='title'><?php echo($res['title']);?></div>
 					<div id='postcontent'><?php echo($res['content']);?></div>
-					<div id='timestamp'><?php echo($res['date'])?></div>
+					<div id='timestamp'><?php echo($res['date'])?></div-->
 				</div>
 				<div id='right'>
 					<!-- ?php include('sidebar.php');?-->
