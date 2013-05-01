66dfd28f715a96e3514e65c5d0f0d34864763586
andyrofl <admin@andyrofl.com>
2013-05-01 09:33:29 -0400

    Holy grail for content. Art pass for right div.

--- index.php
+++ index.php
@@ -19,7 +19,7 @@
 		<link rel=StyleSheet href='styles/main.css' type='text/css'>
 		<?php include('template/header.php');?>
 			<div id='content'>
-				<div id='left'>
+				<div id='left' class='cpiece'>
 					<div id='shortbio'>
 						<?php echo($res[1])?>
 					</div>
@@ -36,16 +36,20 @@
 						?>
 					</div>
 				</div>
-				<div id='right'>
-					<div id='syndicate'>
-						<div class='github icon'></div>
-						<div class='rss icon'><a href='/blog/rss'></a></div>
-						<div class='spotify icon'><a href='spotify'></a></div>
-						<div class='steam icon'><a href='steamcommunity.com/id/andyrofl'></a></div>
-						<div class='twitch icon'><a href='twitch.tv/andyrofl'></a></div>
+				<div id='right' class='cpiece'>
+					<div id='righttop'></div>
+					<div id='rightmid'>
+						<div id='syndicate'>
+							<div class='github icon'></div>
+							<div class='rss icon'><a href='/blog/rss'></a></div>
+							<div class='spotify icon'><a href='spotify'></a></div>
+							<div class='steam icon'><a href='steamcommunity.com/id/andyrofl'></a></div>
+							<div class='twitch icon'><a href='twitch.tv/andyrofl'></a></div>
+							<a class='twitter-timeline' width="280" href='https://twitter.com/AndyroflZZ' data-widget-id='322008644944269313'>Tweets by @AndyroflZZ</a>
+							<script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+"://platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script>
+						</div>
 					</div>
-					<a class='twitter-timeline' href='https://twitter.com/AndyroflZZ' data-widget-id='322008644944269313'>Tweets by @AndyroflZZ</a>
-					<script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+"://platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script>
+					<div id='rightbottom'></div>
 				</div>
 			</div>
 		<?php include('template/footer.php');?>
