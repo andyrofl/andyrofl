79e006d6e70dfd7830b5791273b65e8513be75a4
andyrofl <admin@andyrofl.com>
2013-05-10 13:20:33 -0400

    Sidebar fix

--- blog/sidebar.php
+++ blog/sidebar.php
@@ -3,7 +3,7 @@
 		$recentStmt = $db->prepare('SELECT * FROM blog ORDER BY date DESC LIMIT 3');
 		$recentStmt->execute();
 		while($recents = $recentStmt->fetch()){
-			echo("<div class='recentpost'><span class='rp_title'>".$recents['title']."</span><br/><span class='rp_blurb'>".$recents['description']."</span>");
+			echo("<div class='recentpost'><span class='rp_title'>".$recents['title']."</span><br/><span class='rp_blurb'>".$recents['description']."</span></div>");
 		}
 	?>
 </div>
