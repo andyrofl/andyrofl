ccb1755d6f1e038b11f893a1eb5f4b3de8b3e99e
andyrofl <admin@andyrofl.com>
2013-05-22 00:15:08 -0400

    Footer update

--- template/footer.php
+++ template/footer.php
@@ -5,9 +5,9 @@
 			<a href='http://michelf.ca/projects/php-markdown/'>PHP Markdown</a><br/>
 			<a href='http://ryanfait.com/resources/php-image-resize/'>Image resize</a>
 		</div>
-		<!-- div id='footercontainer'>
+		<div id='footercontainer'>
 			<a href='http://internetdefenseleague.org'><img src='http://internetdefenseleague.org/images/badges/final/footer_badge.png' alt='Member of The Internet Defense League'/></a>
-		</div-->
+		</div>
 	</div>
 	<div id='hfl' class='piece'></div>
 	<div id='hfr' class='piece'></div>
