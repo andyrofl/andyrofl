53ceac357e5bed6f689e61b7df0855589e61a053
andyrofl <admin@andyrofl.com>
2013-05-24 17:25:32 -0400

    footer updates

--- template/footer.php
+++ template/footer.php
@@ -1,13 +1,12 @@
 <div id='footer'>
 	<div id='hfrep' class='piece'>
-		<div id='footercontainer'><br/>
+		<div id='footercontainer'>
+			<a href='http://internetdefenseleague.org'><img src='http://internetdefenseleague.org/images/badges/final/footer_badge.png' alt='Member of The Internet Defense League'/></a>
+		</div>
+		<div id='footercontainer'>
 			<a href='/sitemap.php'>sitemap</a><br/>
 			<a href='http://michelf.ca/projects/php-markdown/'>PHP Markdown</a><br/>
 			<a href='http://ryanfait.com/resources/php-image-resize/'>Image resize</a>
-		</div>
-		<div id='footercontainer'>
-			<br/>
-			<a href='http://internetdefenseleague.org'><img src='http://internetdefenseleague.org/images/badges/final/footer_badge.png' alt='Member of The Internet Defense League'/></a>
 		</div>
 	</div>
 	<div id='hfl' class='piece'></div>
