93ef2f1e8da4f11bdec72d65f2da90b3bd8362d7
andyrofl <admin@andyrofl.com>
2013-05-29 14:29:43 -0400

    Google+

--- arcade/game.php
+++ arcade/game.php
@@ -15,6 +15,7 @@
 		<link rel=StyleSheet href='arcade.css' type='text/css'>
 		<meta charset='utf-8'>
 		<script type="text/javascript"> var _gaq = _gaq || []; _gaq.push(['_setAccount', 'UA-33659175-1']); _gaq.push(['_trackPageview']); (function(){ var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true; ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js'; var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);})();</script>
+		<script type="text/javascript"> window.___gcfg = {lang: 'en-US'}; (function(){ var po = document.createElement('script'); po.type = 'text/javascript'; po.async = true; po.src = 'https://apis.google.com/js/plusone.js'; var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(po, s);})();</script>
 	</head>
 	<body>
 		<div id='main'>
@@ -44,7 +45,15 @@
 												Applet failed to run.  No Java plug-in was found.
 												</object>');
 											}
-											//TODO flash and unity
+											/**
+											 * 0 Unreleased
+											 * 1 Java
+											 * 2 Flash
+											 * 3 Unity
+											 * 4 Silverlight
+											 * 5 Shockwave
+											 * 6 HTML5
+											 */
 										}
 									?>
 								</div>
@@ -58,12 +67,7 @@
 									<div id='info'>
 										<?PHP echo($game['description']);?>
 									</div>
-									<!--div id='share'>
-										##share to fb wall
-										##tweet
-										##G+
-										##Myspace
-									</div-->
+									<?php echo('<g:plusone href="http://andyrofl.com/arcade/game.php?id='.$game['id'].'"></g:plusone>'); ?>
 								</div>
 							</div>
 						</div>
--- blog/post.php
+++ blog/post.php
@@ -40,6 +40,7 @@
 		<link rel=StyleSheet href='blog.css' type='text/css'>
 		<meta charset='utf-8'>
 		<script type="text/javascript"> var _gaq = _gaq || []; _gaq.push(['_setAccount', 'UA-33659175-1']); _gaq.push(['_trackPageview']); (function(){ var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true; ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js'; var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);})();</script>
+		<script type="text/javascript"> window.___gcfg = {lang: 'en-US'}; (function(){ var po = document.createElement('script'); po.type = 'text/javascript'; po.async = true; po.src = 'https://apis.google.com/js/plusone.js'; var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(po, s);})();</script>
 	</head>
 	<body>
 		<div id='main'>
@@ -53,7 +54,8 @@
 									if($valid){
 										echo('<div id="title">'.$post['title'].'</div>
 										<div id="postcontent">'.$post['content'].'</div>
-										<div id="timestamp">'.$post['date'].'</div>');
+										<div id="timestamp">'.$post['date'].'</div>
+										<g:plusone href="http://andyrofl.com/blog/post.php?id='.$post['id'].'"></g:plusone>');
 									}
 									else{
 										echo('Error. No parameter or invalid parameter for post id. <a href="/blog/">Click here</a> to return to blog index');
