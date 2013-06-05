1ecb47504ccd755c48909d2eea06a1dc8719ede6
andyrofl <admin@andyrofl.com>
2013-06-05 15:26:55 -0400

    refactor fixes

--- pages/search.php
+++ pages/search.php
@@ -1,5 +1,6 @@
 <?php
 	session_start();
+	include('../sql.php');
 ?>
 <!DOCTYPE HTML>
 <html>
--- template/footer.php
+++ template/footer.php
@@ -4,7 +4,7 @@
 			<a href='http://internetdefenseleague.org'><img src='http://internetdefenseleague.org/images/badges/final/footer_badge.png' alt='Member of The Internet Defense League'/></a>
 		</div>
 		<div id='footercontainer'>
-			<a href='/sitemap.php'>sitemap</a><br/>
+			<a href='/sitemap'>sitemap</a><br/>
 			<a href='http://michelf.ca/projects/php-markdown/'>PHP Markdown</a><br/>
 			<a href='http://ryanfait.com/resources/php-image-resize/'>Image resize</a>
 		</div>
