e53a9120442f4f9fb90abf301744a1e1de746341
andyrofl <admin@andyrofl.com>
2013-06-11 17:42:40 -0400

    consolidate google scripts

---
+++ scripts/google.js
@@ -0,0 +1,19 @@
+var _gaq = _gaq || [];
+_gaq.push(['_setAccount', 'UA-33659175-1']);
+_gaq.push(['_trackPageview']);
+(function(){
+	var ga = document.createElement('script');
+	ga.type = 'text/javascript';
+	ga.async = true; ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+	var s = document.getElementsByTagName('script')[0];
+	s.parentNode.insertBefore(ga, s);})();
+
+(function() {
+	var cx = '011954134038849293733:lw4z0ujey7q';
+	var gcse = document.createElement('script');
+	gcse.type = 'text/javascript';
+	gcse.async = true;
+	gcse.src = (document.location.protocol == 'https:' ? 'https:' : 'http:') + '//www.google.com/cse/cse.js?cx=' + cx;
+	var s = document.getElementsByTagName('script')[0];
+	s.parentNode.insertBefore(gcse, s);
+})();
