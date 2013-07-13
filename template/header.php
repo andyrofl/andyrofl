d3ac108b565f0dd3c597f0671df844df73da4391
andy rofl <admin@andyrofl.com>
2013-07-12 23:26:15 -0400

    move google scripts

--- template/header.php
+++ template/header.php
@@ -60,19 +60,7 @@
 		}
 		return false;
 	}
-	
 ?>
-<script>
-	(function() {
-		var cx = '011954134038849293733:lw4z0ujey7q';
-		var gcse = document.createElement('script');
-		gcse.type = 'text/javascript';
-		gcse.async = true;
-		gcse.src = (document.location.protocol == 'https:' ? 'https:' : 'http:') + '//www.google.com/cse/cse.js?cx=' + cx;
-		var s = document.getElementsByTagName('script')[0];
-		s.parentNode.insertBefore(gcse, s);
-	})();
-</script>
 <div id='header'>
 	<div id='hfrep' class='piece'>
 		<div id='largelinks'>
@@ -104,7 +92,7 @@
 					}
 					else{
 						echo('<gcse:searchbox-only></gcse:searchbox-only>');
-						echo("<script type='text/javascript'> var _gaq = _gaq || []; _gaq.push(['_setAccount', 'UA-33659175-1']); _gaq.push(['_trackPageview']); (function(){ var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true; ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js'; var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);})();</script><script type='text/javascript'> window.___gcfg = {lang: 'en-US'}; (function(){ var po = document.createElement('script'); po.type = 'text/javascript'; po.async = true; po.src = 'https://apis.google.com/js/plusone.js'; var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(po, s);})();</script>");
+						echo("<script>(function(){ var cx = '011954134038849293733:lw4z0ujey7q'; var gcse = document.createElement('script'); gcse.type = 'text/javascript'; gcse.async = true; gcse.src = (document.location.protocol == 'https:' ? 'https:' : 'http:') + '//www.google.com/cse/cse.js?cx=' + cx; var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(gcse, s);})();</script><script type='text/javascript'> var _gaq = _gaq || []; _gaq.push(['_setAccount', 'UA-33659175-1']); _gaq.push(['_trackPageview']); (function(){ var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true; ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js'; var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);})();</script><script type='text/javascript'> window.___gcfg = {lang: 'en-US'}; (function(){ var po = document.createElement('script'); po.type = 'text/javascript'; po.async = true; po.src = 'https://apis.google.com/js/plusone.js'; var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(po, s);})();</script>");
 					}
 				?>
 			</div>
