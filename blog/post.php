976f4451b74af492ce73866474fecc783bbc9689
andy rofl <admin@andyrofl.com>
2013-07-12 22:48:14 -0400

    move googl scripts to header in a do-not-track conditional

--- blog/post.php
+++ blog/post.php
@@ -39,8 +39,6 @@
 		<link rel=StyleSheet href='/styles/main.css' type='text/css'>
 		<link rel=StyleSheet href='/styles/blog.css' type='text/css'>
 		<meta charset='utf-8'>
-		<script type="text/javascript"> var _gaq = _gaq || []; _gaq.push(['_setAccount', 'UA-33659175-1']); _gaq.push(['_trackPageview']); (function(){ var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true; ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js'; var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);})();</script>
-		<script type="text/javascript"> window.___gcfg = {lang: 'en-US'}; (function(){ var po = document.createElement('script'); po.type = 'text/javascript'; po.async = true; po.src = 'https://apis.google.com/js/plusone.js'; var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(po, s);})();</script>
 	</head>
 	<body>
 		<div id='main'>
