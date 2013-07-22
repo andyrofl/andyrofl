5711635415edcee9e44d3c7f984b882b202ece41
andy rofl <admin@andyrofl.com>
2013-07-22 17:10:04 -0400

    whitespace...

--- admin/inventory.php
+++ admin/inventory.php
@@ -16,53 +16,53 @@
 			echo('something went wrong. SQL server reporting'.$invResult.'changed rows.');
 		}
 	}
-	?>
-	<!DOCTYPE HTML>
-	<html>
-		<head>
-			<title>admin | inventory</title>
-			<link rel=StyleSheet href='../styles/main.css' type='text/css'>
-			<link rel=StyleSheet href='admin.css' type='text/css'>
-			<meta charset='utf-8'>
-		</head>
-		<body>
-			<div id='main'>
-				<?php include('../template/header.php');?>
-				<div id='content'>
-					<div id='left' class='piece'>
-						<div id='ctop'><div id='ctoprep' class='piece'></div><div id='ctopl' class='piece'></div></div>
-						<div id='cmid'>
-							<div id='cmidrep'>
-								<?php
-									if(!$_SESSION['login']){
-										echo("invalid credentials. <a href='/admin/'>return to admin panel.</a></div></div><div id='cmidl'></div><div id='cbot'><div id='cbotrep'></div><div id='cbotl'></div></div></div><div id='right' class='piece'><div id='righttop'></div><div id='rightmid'></div><div id='rightbottom'></div></div></div>");
-										include('../template/footer.php');
-										echo('</div></body></html>');
-										exit;
-									}
-								?>
-								<form action='/admin/inventory' method='post'>
-									<input type='text' name='item' value='item'/>
-									<input type='text' name='location' value='location'/>
-									<input type='text' name='serial' value='serial/cd-key'/>
-									<input type='text' name='category' value='category'/>
-									<input type='text' name='value' value='value'></input>
-									<input type='submit' value='submit'/>
-								</form>
-							</div>
+?>
+<!DOCTYPE HTML>
+<html>
+	<head>
+		<title>admin | inventory</title>
+		<link rel=StyleSheet href='../styles/main.css' type='text/css'>
+		<link rel=StyleSheet href='admin.css' type='text/css'>
+		<meta charset='utf-8'>
+	</head>
+	<body>
+		<div id='main'>
+			<?php include('../template/header.php');?>
+			<div id='content'>
+				<div id='left' class='piece'>
+					<div id='ctop'><div id='ctoprep' class='piece'></div><div id='ctopl' class='piece'></div></div>
+					<div id='cmid'>
+						<div id='cmidrep'>
+							<?php
+								if(!$_SESSION['login']){
+									echo("invalid credentials. <a href='/admin/'>return to admin panel.</a></div></div><div id='cmidl'></div><div id='cbot'><div id='cbotrep'></div><div id='cbotl'></div></div></div><div id='right' class='piece'><div id='righttop'></div><div id='rightmid'></div><div id='rightbottom'></div></div></div>");
+									include('../template/footer.php');
+									echo('</div></body></html>');
+									exit;
+								}
+							?>
+							<form action='/admin/inventory' method='post'>
+								<input type='text' name='item' value='item'/>
+								<input type='text' name='location' value='location'/>
+								<input type='text' name='serial' value='serial/cd-key'/>
+								<input type='text' name='category' value='category'/>
+								<input type='text' name='value' value='value'></input>
+								<input type='submit' value='submit'/>
+							</form>
 						</div>
-						<div id='cmidl'></div>
-						<div id='cbot'><div id='cbotrep'></div><div id='cbotl'></div></div>
 					</div>
-					<div id='right' class='piece'>
-						<div id='righttop'></div>
-							<div id='rightmid'>
-								<?php include('sidebar.php');?>
-							</div>
-						<div id='rightbottom'></div>
-					</div>
+					<div id='cmidl'></div>
+					<div id='cbot'><div id='cbotrep'></div><div id='cbotl'></div></div>
 				</div>
-				<?php include('../template/footer.php');?>
+				<div id='right' class='piece'>
+					<div id='righttop'></div>
+						<div id='rightmid'>
+							<?php include('sidebar.php');?>
+						</div>
+					<div id='rightbottom'></div>
+				</div>
 			</div>
-		</body>
-	</html>
+			<?php include('../template/footer.php');?>
+		</div>
+	</body>
+</html>
