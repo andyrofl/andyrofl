22e812a304293152d127d99be978ce81ee70fbda
andy rofl <admin@andyrofl.com>
2013-07-22 12:53:46 -0400

    move admin links to a separate sidebar file

---
+++ admin/sidebar.php
@@ -0,0 +1,5 @@
+<a href='blog.php'>blog</a><br/>
+<a href='inventory.php'>inventory</a><br/>
+<a href='portfolio.php'>portfolio</a><br/>
+<a href='users.php'>manage users</a><br/>
+<a href='database.php'>manage database</a>
