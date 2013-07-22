19e475f90935ab037e956d64412b4aca74b28116
andy rofl <admin@andyrofl.com>
2013-07-22 15:54:53 -0400

    update sidebar

--- admin/sidebar.php
+++ admin/sidebar.php
@@ -1,6 +1,6 @@
+<a href='/admin/home'>home</a><br/>
 <a href='/admin/blog'>blog</a><br/>
-<a href='/admin/inventory'>inventory</a><br/>
 <a href='/admin/portfolio'>portfolio</a><br/>
 <a href='/admin/users'>manage users</a><br/>
-<a href='/admin/database'>manage database</a>
-<a href='/admin/home'>manage database</a>
+<a href='/admin/database'>manage database</a><br/>
+<a href='/admin/inventory'>inventory</a>
